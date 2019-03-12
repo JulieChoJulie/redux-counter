1b2f6b7cb974af066ac5c97b47c078c808dd0037
JulieChoJulie <h32cho@gmail.com>
2019-03-12 16:47:01 -0400

    Buttons component added

---
+++ src/components/Buttons.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Buttons.css';
+
+const propTypes = {
+    onCreate: PropTypes.func,
+    onRemove: PropTypes.func,
+};
+const defaultProps = {
+    onCreate: () => console.warn('onCreate not defined'),
+    onRemove: () => console.warn('onRemove not defined')
+};
+
+const Buttons = ({onCreate, onRemove}) => {
+    return (
+        <div className="buttons">
+            <div className="btn add" onClick={onCreate}>
+                Create
+            </div>
+            <div classNAme="btn remove" onClick={onRemove}>
+                Remove
+            </div>
+        </div>
+    )
+};
+
+Buttons.propTypes = propTypes;
+Buttons.defaultProps = defaultProps;
+export default Buttons;
