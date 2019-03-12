3ef45379de7726e784df58c86e5f48b559351bd0
JulieChoJulie <h32cho@gmail.com>
2019-03-12 16:21:22 -0400

    actions(create and remove) added

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -10,6 +10,9 @@ and import these values
 whenever we need to use them in different files
  */
 
+export const CREATE = 'CREATE';
+export const REMOVE = 'REMOVE';
+
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 export const SET_COLOR = 'SET_COLOR';
--- src/actions/index.js
+++ src/actions/index.js
@@ -4,16 +4,28 @@ action creator: create action obj
 
 import * as types from './ActionTypes';
 
-export const increment = () => ({
-    type: types.INCREMENT
-});
-
-export const decrement = () => ({
-    type: types.DECREMENT
-});
-
-
-export const setColor= (color) => ({
-    type: types.SET_COLOR,
+export const create = (color) => ({
+    type: types.CREATE,
     color
+});
+
+export const remove = () => ({
+    type: types.REMOVE,
+});
+
+export const increment = (index) => ({
+    type: types.INCREMENT,
+    index
+});
+
+export const decrement = (index) => ({
+    type: types.DECREMENT,
+    index
+});
+
+
+export const setColor= ({index, color}) => ({
+    type: types.SET_COLOR,
+    color,
+    index
 });
