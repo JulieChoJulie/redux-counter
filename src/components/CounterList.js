b51d9655a8645539819aa2ac520e2928700a4070
JulieChoJulie <h32cho@gmail.com>
2019-03-12 20:01:57 -0400

    immutable.js used

--- src/components/CounterList.js
+++ src/components/CounterList.js
@@ -1,90 +1,45 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import Counter from './Counter';
-// import './CounterList.css';
-//
-// const propTypes = {
-//     counters: PropTypes.arrayOf(PropTypes.shape({
-//         color: PropTypes.string,
-//         number: PropTypes.number
-//     })),
-//     onIncrement: PropTypes.func,
-//     onDecrement: PropTypes.func,
-//     onSetColor: PropTypes.func,
-// };
-// const defaultProps = {
-//     counters: [],
-//     onIncrement: () => console.warn('onIncrement not defined'),
-//     onDecrement: () => console.warn('onDecrement not defined')
-// };
-//
-// const CounterList = ({counters, onIncrement, onDecrement, onSetColor}) => {
-//     const counterList =  counters.map(({counter, i}) => {
-//         return <Counter
-//             key={i}
-//             index={i}
-//             {...counter}
-//             onIncrement={onIncrement}
-//             onDecrement={onDecrement}
-//             onSetColor={onSetColor}/>
-//     });
-//
-//     return (
-//         <div className="CounterList">
-//             {counterList}
-//         </div>
-//     )
-// };
-//
-// CounterList.propTypes = propTypes;
-// CounterList.defaultProps = defaultProps;
-//
-// export default CounterList;
-
 import React from 'react';
 import Counter from './Counter';
 import PropTypes from 'prop-types';
+import { List } from 'immutable';
 
 import './CounterList.css';
 
+
+
+const propTypes = {
+    counters: PropTypes.instanceOf(List),
+    onIncrement: PropTypes.func,
+    onDecrement: PropTypes.func,
+    onSetColor: PropTypes.func,
+};
+const defaultProps = {
+    counters: [],
+    onIncrement: () => console.warn('onIncrement not defined'),
+    onDecrement: () => console.warn('onDecrement not defined')
+};
+
 const CounterList = ({counters, onIncrement, onDecrement, onSetColor}) => {
 
-    const counterList = counters.map(
+    const counterList =  counters.map(
         (counter, i) => (
             <Counter
-                key={i}
-                index={i}
-                {...counter}
-                onIncrement={onIncrement}
-                onDecrement={onDecrement}
-                onSetColor={onSetColor}
-            />
-        )
-    );
+            key={i}
+            index={i}
+            {...counter.toJS()}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
+            onSetColor={onSetColor}/>
+        ));
 
     return (
         <div className="CounterList">
             {counterList}
         </div>
-    );
+    )
 };
 
-CounterList.propTypes = {
-    counters: PropTypes.arrayOf(PropTypes.shape({
-        color: PropTypes.string,
-        number: PropTypes.number
-    })),
-    onIncrement: PropTypes.func,
-    onDecrement: PropTypes.func,
-    onSetColor: PropTypes.func
-};
-
-CounterList.defaultProps = {
-    counters: [],
-    onIncrement: () => console.warn('onIncrement not defined'),
-    onDecrement: () => console.warn('onDecrement not defined'),
-    onSetColor: () => console.warn('onSetColor not defined')
-}
-
+CounterList.propTypes = propTypes;
+CounterList.defaultProps = defaultProps;
 
 export default CounterList;
